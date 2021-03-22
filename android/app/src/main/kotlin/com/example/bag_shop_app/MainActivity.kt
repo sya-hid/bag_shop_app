f3ccfd71844b4e534d096472b5d007ac11715902
Sya-Hid <syarifhidayatullah2020@gmail.com>
2021-03-22 18:22:24 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bag_shop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bag_shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bag_shop_app/constants.dart';
+import 'package:bag_shop_app/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        title: 'Flutter Demo',
+        home: HomeScreen());
+  }
+}
